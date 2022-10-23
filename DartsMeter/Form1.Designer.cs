a25070f7e6b90b8bbf5d249d428738d20901ec61
Piotr Szamocki <szamocki.piotr04@gmail.com>
2022-10-23 18:09:17 +0200

    Added test image and function to read pixel value from it.

--- DartsMeter/Form1.Designer.cs
+++ DartsMeter/Form1.Designer.cs
@@ -28,21 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(220, 78);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(357, 300);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- DartsMeter/Form1.cs
+++ DartsMeter/Form1.cs
@@ -1,17 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DartsMeter
 {
     public partial class Form1 : Form
     {
+        Bitmap bitmap;  // Declaring beatmap
+
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +15,13 @@ namespace DartsMeter
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Console.WriteLine("działa");
+            bitmap = new Bitmap(pictureBox1.ClientSize.Width, pictureBox1.Height);  // Creating new bitmap
+            pictureBox1.DrawToBitmap(bitmap, pictureBox1.ClientRectangle);
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Color color = bitmap.GetPixel(e.X, e.Y);  // Gets pixel value from point where cursor is
         }
     }
 }
