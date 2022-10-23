c683d8efee5902d497c7c4f3517cc2160fb9c8fe
Piotr Szamocki <szamocki.piotr04@gmail.com>
2022-10-23 22:51:58 +0200

    Added dartboard and checking point system.

--- DartsMeter/Form1.Designer.cs
+++ DartsMeter/Form1.Designer.cs
@@ -28,21 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(220, 78);
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::DartsMeter.Properties.Resources.tarcza_bezalfy;
+            this.pictureBox1.Location = new System.Drawing.Point(215, 25);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(357, 300);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(696, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -50,6 +61,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -61,6 +73,8 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- DartsMeter/Form1.cs
+++ DartsMeter/Form1.cs
@@ -15,13 +15,88 @@ namespace DartsMeter
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            bitmap = new Bitmap(pictureBox1.ClientSize.Width, pictureBox1.Height);  // Creating new bitmap
-            pictureBox1.DrawToBitmap(bitmap, pictureBox1.ClientRectangle);
+            bitmap = (Bitmap)Image.FromFile(@"C:\Users\Altin\source\repos\DartsMeter\DartsMeter\Properties\Sources\Images\tarcza_alfa.png");  // Creating new bitmap
+            bitmap = new Bitmap(bitmap, new Size(pictureBox1.ClientSize.Width, pictureBox1.ClientSize.Height));  
+
         }
 
         private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
         {
             Color color = bitmap.GetPixel(e.X, e.Y);  // Gets pixel value from point where cursor is
+
+            Console.WriteLine(calculateCurrentPoint(color));
+        }
+
+        private string calculateCurrentPoint(Color color)  // Returns point value based on argb value
+        {
+            switch (color.A)
+            {
+                case 255: return "single 20";
+                case 253: return "single 1";
+                case 250: return "single 18";
+                case 248: return "single 4";
+                case 246: return "single 13";
+                case 244: return "single 6";
+                case 242: return "single 10";
+                case 239: return "single 15";
+                case 237: return "single 2";
+                case 235: return "single 17";
+                case 233: return "single 3";
+                case 231: return "single 19";
+                case 229: return "single 7";
+                case 226: return "single 16";
+                case 224: return "single 8";
+                case 222: return "single 11";
+                case 220: return "single 14";
+                case 218: return "single 9";
+                case 216: return "single 12";
+                case 214: return "single 5";
+                case 212: return "double 20";
+                case 210: return "double 1";
+                case 208: return "double 18";
+                case 206: return "double 4";
+                case 204: return "double 13";
+                case 202: return "double 6";
+                case 200: return "double 10";
+                case 198: return "double 15";
+                case 196: return "double 2";
+                case 194: return "double 17";
+                case 192: return "double 3";
+                case 190: return "double 19";
+                case 188: return "double 7";
+                case 186: return "double 16";
+                case 184: return "double 8";
+                case 183: return "double 11";
+                case 181: return "double 14";
+                case 179: return "double 9";
+                case 177: return "double 12";
+                case 175: return "double 5";
+                case 173: return "triple 20";
+                case 171: return "triple 1";
+                case 170: return "triple 18";
+                case 168: return "triple 4";
+                case 166: return "triple 13";
+                case 164: return "triple 6 ";
+                case 163: return "triple 10";
+                case 161: return "triple 15";
+                case 159: return "triple 2";
+                case 157: return "triple 17";
+                case 156: return "triple 3";
+                case 154: return "triple 19";
+                case 152: return "triple 7";
+                case 151: return "triple 16";
+                case 149: return "triple 8";
+                case 147: return "triple 11";
+                case 146: return "triple 14";
+                case 144: return "triple 9";
+                case 142: return "triple 12";
+                case 141: return "triple 5";
+                case 139: return "single 25";
+                case 138: return "double 25";
+                case 136: return "miss 0";
+
+                default: return "miss 0";
+            }
         }
     }
 }
