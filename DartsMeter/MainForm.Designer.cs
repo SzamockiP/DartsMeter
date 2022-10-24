bc5d4621646860503e93bdd2ad405d31924e808b
Piotr Szamocki <szamocki.piotr04@gmail.com>
2022-10-25 00:28:00 +0200

    Changed function names and optimised things.

--- DartsMeter/MainForm.Designer.cs
+++ DartsMeter/MainForm.Designer.cs
@@ -28,40 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.PB_dartBoard = new System.Windows.Forms.PictureBox();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_dartBoard)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // PB_dartBoard
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.Image = global::DartsMeter.Properties.Resources.tarcza_bezalfy;
-            this.pictureBox1.Location = new System.Drawing.Point(281, 147);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.PB_dartBoard.BackColor = System.Drawing.Color.Transparent;
+            this.PB_dartBoard.Image = global::DartsMeter.Properties.Resources.tarcza_bezalfy;
+            this.PB_dartBoard.Location = new System.Drawing.Point(281, 147);
+            this.PB_dartBoard.Name = "PB_dartBoard";
+            this.PB_dartBoard.Size = new System.Drawing.Size(400, 400);
+            this.PB_dartBoard.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PB_dartBoard.TabIndex = 0;
+            this.PB_dartBoard.TabStop = false;
+            this.PB_dartBoard.MouseClick += new System.Windows.Forms.MouseEventHandler(this.PB_dartBoard_MouseClick);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(984, 561);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.PB_dartBoard);
             this.Name = "MainForm";
             this.Text = "DartsMeter";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PB_dartBoard)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox PB_dartBoard;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
